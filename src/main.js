9eb3e1fb9b0c56c9fedc45e48b3b6942c09b79a2
Cephas Tokoli <matumasie@gmail.com>
2019-10-27 23:15:50 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,13 +11,13 @@ Vue.use(Router);
 const routers = new Router({router});
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAn1hyYx91utL5qtqnQY9RU4Xrf030s3FQ",
-  authDomain: "vue-social-meme.firebaseapp.com",
-  databaseURL: "https://vue-social-meme.firebaseio.com",
-  projectId: "vue-social-meme",
-  storageBucket: "vue-social-meme.appspot.com",
-  messagingSenderId: "857799278176",
-  appId: "1:857799278176:web:28bcff3c47168e86808d05"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
